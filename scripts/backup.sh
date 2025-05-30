cad5ff783481045fa68633eec52b8190b92386cb
Cameron MacMillan <camacmi01@gmail.com>
2025-05-30 17:40:04 -0400

    cleaned up

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -24,6 +24,4 @@ rm $HOME/projects/dotfiles/aur_pkglist.txt
 pacman -Qn > $HOME/projects/dotfiles/aor_pkglist.txt
 pacman -Qm > $HOME/projects/dotfiles/aur_pkglist.txt
 
-rm $HOME/projects/dotfiles/.config/ranger/ranger/plugins/ranger_devicons/.gitignore
-rm $HOME/projects/dotfiles/.config/ranger/ranger_nopreview/plugins/ranger_devicons/.gitignore
 exit
