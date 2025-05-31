a874359b9afe871417fd8d3afedf8b8db42c3f0a
Cameron MacMillan <camacmi01@gmail.com>
2025-05-31 02:12:32 -0400

    fixed package list

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -21,7 +21,7 @@ cp -ru $HOME/.config/waybar $HOME/projects/dotfiles/.config
 
 rm $HOME/projects/dotfiles/aor_pkglist.txt
 rm $HOME/projects/dotfiles/aur_pkglist.txt
-pacman -Qn > $HOME/projects/dotfiles/aor_pkglist.txt
+pacman -Qqen > $HOME/projects/dotfiles/aor_pkglist.txt
 pacman -Qm > $HOME/projects/dotfiles/aur_pkglist.txt
 
 exit
